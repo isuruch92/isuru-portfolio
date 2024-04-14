c95f7f29d6f70ce040ebf061a7aa34a6f3d288c5
isuruch92 <isuruch92@gmail.com>
2024-04-14 15:11:13 +0300

    Header component

---
+++ components/header.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { links } from "@/lib/data";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-xl shadow-black/[0.03] 
+      backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        initial={{ x: "-50%", y: -100, opacity: 0 }}
+        animate={{ x: "-50%", y: 0, opacity: 1 }}
+        transition={{
+          type: "spring",
+          duration: 0.6,
+          stiffness: 180,
+        }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                type: "spring",
+                duration: 0.6,
+                stiffness: 180,
+              }}
+            >
+              <Link
+                className="w-full flex items-center justify-center px-3 py-3 hover:text-slate-950 transition duration-300"
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
