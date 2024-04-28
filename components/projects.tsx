06395bb0be126b94a3bfcdf6aab3c915af35839f
isuruch92 <isuruch92@gmail.com>
2024-04-29 01:02:10 +0300

    Skills section

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
   const ref = useSectionInView("Projects", 0.5);
 
   return (
-    <section id="projects" ref={ref} className="scroll-mt-32">
+    <section id="projects" ref={ref} className="scroll-mt-32 mb-28 sm:mb-40">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
